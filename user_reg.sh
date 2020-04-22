a78fba0739d54f8a0093d3cb4f6018c837135436
Rajesh-starlord <rajeshkanhar.rk10@gmail.com>
2020-04-22 20:47:36 +0530

    added to master

---
+++ user_reg.sh
@@ -0,0 +1,11 @@
+#! /bin/bash -x
+#validating user first name
+
+read -p "enter First name---" first_name
+fname_pat="^([A-Z]{1}){3,}$"
+if [[ $first_name =~ $fname_pat ]];
+then
+	echo "first name successfully registered:"
+else
+	echo "enter a valid first name:"
+fi
