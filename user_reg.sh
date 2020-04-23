117c00b366686fdf7c840423c09c41ce5a630ab2
Rajesh-starlord <56430104+Rajesh-starlord@users.noreply.github.com>
2020-04-23 08:44:55 +0530

    Update user_reg.sh

--- user_reg.sh
+++ user_reg.sh
@@ -6,7 +6,7 @@ fname_pat="^([A-Z]{1}){3,}$"
 read -p "enter Last name---" Last_name
 Lname_pat="^([A-Z]{1,}){3,}"
 read -p "enter your email--" email
-email_pat="^([a-zA-Z])?[0-9]*[_\+\.0-9a-zA-Z]+[@][0-9a-zA-Z_\+\.]+[co|in|com|au]$"
+email_pat="^([a-zA-Z])*[_\+\.\-]*[0-9a-zA-Z]+[@][0-9a-zA-Z_\+\.]+[co|in|com|au]$"
 
 if [[ $first_name =~ $fname_pat ]];
 then
