a30ef674788471c33dfe38c742565b1691f20bf1
Rajesh-starlord <56430104+Rajesh-starlord@users.noreply.github.com>
2020-04-23 09:13:47 +0530

    Update user_reg.sh

--- user_reg.sh
+++ user_reg.sh
@@ -6,7 +6,7 @@ fname_pat="^([A-Z]{1}){3,}$"
 read -p "enter Last name---" Last_name
 Lname_pat="^([A-Z]{1,}){3,}"
 read -p "enter your email--" email
-email_pat="^([a-zA-Z])?[0-9]*[_\+\.0-9a-zA-Z]+[@][0-9a-zA-Z_\+\.]+[co|in|com|au]$"
+email_pat="^([a-zA-Z])+[_\+\.\-]*[0-9a-zA-Z]+[@][0-9a-zA-Z_\+\.]+[co|in|com|au]$"
 read -p "enter phone no ---" phone
 phone_pat="^((\+){1}91)[ ]{1}[1-9]{1}[0-9]{9}$"
 
