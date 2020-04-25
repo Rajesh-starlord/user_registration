ae1c64d1142d944080f483bd2e1fef3f9958fb88
Rajesh-starlord <56430104+Rajesh-starlord@users.noreply.github.com>
2020-04-25 12:06:14 +0530

    Update user_reg.sh

--- user_reg.sh
+++ user_reg.sh
@@ -2,7 +2,7 @@
 #validating user first name
 
 read -p "enter First name---" first_name
-fname_pat="^([A-Z]{1}){3,}$"
+fname_pat="^([A-Z]{1})+[a-z]{2,}$"
 if [[ $first_name =~ $fname_pat ]];
 then
 	echo "first name successfully registered:"
